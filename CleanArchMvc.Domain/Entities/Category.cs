956b167ff476a81d4c8ad30476aa9b5f4889d0f6
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-21 13:59:26 -0300

    Add project files.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // No projeto, uma categoria podera ter mais de um produto. Vamos setar a seguinte propriedade.
+        public ICollection<Product> Products{ get; set; }   
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+        // Adicionamos no modelo product uma referencia (chave estrangeira) a categoria, essas propriedades de navegação sao 
+        // para que o ef core que crie uma tabela intermediaria entre Category e Product, ja que há uma relação n:1.
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
