27bee8172a1615bd07dd419f2d4a8c6e5fa6431a
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-21 15:43:27 -0300

    feat: add domain models Product and Category

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,42 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category // O modificador sealed define que a classe em questão não pode ser herdada.
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public static readonly string ERROR_INVALID_NAME = "Invalid name.Name is required.";
+        public static readonly string ERROR_MINIMUM_CHAR_NAME = "Invalid name, too short, minimum 3 charecters.";
+        public static readonly string ERROR_MINIMUM_ID_VALUE = "Invalid Id value.";
 
-        // No projeto, uma categoria podera ter mais de um produto. Vamos setar a seguinte propriedade.
-        public ICollection<Product> Products{ get; set; }   
+        public static readonly int MINIMUM_CHAR_NAME = 3;
+        public static readonly int MINIMUM_ID_VALUE = 0;
+
+        // PROPERTIES
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        // CONSTRUCTOR
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        
+        public Category(int id, string name) // Adicionamos mais um construtor pois será necessário no momento de popular o bd.
+        {
+            DomainExceptionValidation.When(id < MINIMUM_ID_VALUE, ERROR_MINIMUM_ID_VALUE);
+            Id = id;
+            ValidateDomain(name);
+        }
+        
+        // PUBLIC METHODS
+        public ICollection<Product> Products{ get; set; }  // No projeto, uma categoria podera ter mais de um produto. Vamos setar a seguinte propriedade.
+
+        // PRIVATE METHODS
+        private void ValidateDomain(string name) // Adicionando validadores no dominio de categoria.
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), ERROR_INVALID_NAME);
+            DomainExceptionValidation.When(name.Length < MINIMUM_CHAR_NAME, ERROR_MINIMUM_CHAR_NAME);
+
+            Name = name;
+        }
     }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -6,18 +6,21 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Product
+    public sealed class Product // o modificador sealed define que a classe em questão não pode ser herdada.
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
+        // PROPERTIES
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
 
-        // Adicionamos no modelo product uma referencia (chave estrangeira) a categoria, essas propriedades de navegação sao 
-        // para que o ef core que crie uma tabela intermediaria entre Category e Product, ja que há uma relação n:1.
+        
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        // Adicionamos no modelo product uma referencia (chave estrangeira) a categoria
+        // essas propriedades de navegação sao para que o ef core que crie uma tabela intermediaria entre Category e Product,
+        // ja que há uma relação n:1.
     }
 }
