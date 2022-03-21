b84bb4a1cb07ad2af7a1dcb97f40e769ec4d188a
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-21 18:57:36 -0300

    feat: add interfaces ICategoryRepository and IProductRepository

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync(); // o Task<> confirma que o método assincrono
+        Task<Category> GetCategoryByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    internal interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync(); // o Task<> confirma que o método assincrono
+        Task<Product> GetProductByIdAsync(int? id);
+        Task<Product> GetProductCategoryByIdAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
