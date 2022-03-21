5fded96abd2c2cb2cdf1c4f5e1efab939de74f9f
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-21 18:37:22 -0300

    feat: update product e category models with more validations

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,26 +1,15 @@
 ﻿using CleanArchMvc.Domain.Validation;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
     public sealed class Category // O modificador sealed define que a classe em questão não pode ser herdada.
     {
-        public static readonly string ERROR_INVALID_NAME = "Invalid name.Name is required.";
-        public static readonly string ERROR_MINIMUM_CHAR_NAME = "Invalid name, too short, minimum 3 charecters.";
-        public static readonly string ERROR_MINIMUM_ID_VALUE = "Invalid Id value.";
 
-        public static readonly int MINIMUM_CHAR_NAME = 3;
-        public static readonly int MINIMUM_ID_VALUE = 0;
-
-        // PROPERTIES
         public int Id { get; private set; }
         public string Name { get; private set; }
+        public ICollection<Product> Products{ get; set; }  // No projeto, uma categoria podera ter mais de um produto. Vamos setar a seguinte propriedade.
 
-        // CONSTRUCTOR
         public Category(string name)
         {
             ValidateDomain(name);
@@ -28,19 +17,23 @@ namespace CleanArchMvc.Domain.Entities
         
         public Category(int id, string name) // Adicionamos mais um construtor pois será necessário no momento de popular o bd.
         {
-            DomainExceptionValidation.When(id < MINIMUM_ID_VALUE, ERROR_MINIMUM_ID_VALUE);
+            DomainExceptionValidation.When(id < Constants.Constants.ZERO_VALUE, Constants.Constants.ERROR_MINIMUM_ID_VALUE);
             Id = id;
             ValidateDomain(name);
         }
         
-        // PUBLIC METHODS
-        public ICollection<Product> Products{ get; set; }  // No projeto, uma categoria podera ter mais de um produto. Vamos setar a seguinte propriedade.
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
 
-        // PRIVATE METHODS
+
         private void ValidateDomain(string name) // Adicionando validadores no dominio de categoria.
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), ERROR_INVALID_NAME);
-            DomainExceptionValidation.When(name.Length < MINIMUM_CHAR_NAME, ERROR_MINIMUM_CHAR_NAME);
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), Constants.Constants.ERROR_INVALID_NAME);
+            DomainExceptionValidation.When(name.Length < 
+                Constants.Constants.MINIMUM_CHAR_NAME_VALUE, Constants.Constants.ERROR_MINIMUM_CHAR_NAME
+            );
 
             Name = name;
         }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
     public sealed class Product // o modificador sealed define que a classe em questão não pode ser herdada.
     {
-        // PROPERTIES
         public int Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
@@ -22,5 +17,46 @@ namespace CleanArchMvc.Domain.Entities
         // Adicionamos no modelo product uma referencia (chave estrangeira) a categoria
         // essas propriedades de navegação sao para que o ef core que crie uma tabela intermediaria entre Category e Product,
         // ja que há uma relação n:1.
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image); 
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < Constants.Constants.ZERO_VALUE, Constants.Constants.ERROR_MINIMUM_ID_VALUE);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image) // Adicionando validadores no dominio de produto.
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), Constants.Constants.ERROR_INVALID_NAME);
+            DomainExceptionValidation.When(
+                name.Length < Constants.Constants.MINIMUM_CHAR_NAME_VALUE, Constants.Constants.ERROR_MINIMUM_CHAR_NAME
+            );
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), Constants.Constants.ERROR_INVALID_DESCRIPTION);
+            DomainExceptionValidation.When(
+                description.Length < Constants.Constants.MINIMUM_CHAR_DESCRIPTION_VALUE, Constants.Constants.ERROR_MINIMUM_CHAR_DESCRIPTION
+            );
+
+            DomainExceptionValidation.When(price < Constants.Constants.ZERO_VALUE, Constants.Constants.ERROR_MINIMUM_PRICE_VALUE);
+            DomainExceptionValidation.When(stock < Constants.Constants.ZERO_VALUE, Constants.Constants.ERROR_MINIMUM_STOCK_VALUE);
+
+            DomainExceptionValidation.When(
+                image.Length > Constants.Constants.MAXIMUM_CHAR_IMAGE_VALUE, Constants.Constants.ERROR_MAXIMUM_CHAR_IMAGE
+            );
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
     }
 }
