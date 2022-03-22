a4c0a84d4b68827f51fdb24b57ef7e68546abb93
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-22 10:15:42 -0300

    feat(infra): add class Context Database and dependencies to connect entity framework with SQLServer datatabase

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    // Essa é a classe de contexto, com ela vamos definir a conexão com o banco de dados usando o entity framework
+    public class ApplicationDbContext: DbContext
+    {
+        // Fazendo o mapeampento ORM das entidades
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Definindo as configurações das entidades
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
