dc4173f96a19d5cc2b3d442b76033bae26f017f8
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-22 10:16:14 -0300

    feat(infra): add entities to model for database migration

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletronicos"),
+                new Category(3, "Acessorios")
+                );
+        }
+    }
+}
