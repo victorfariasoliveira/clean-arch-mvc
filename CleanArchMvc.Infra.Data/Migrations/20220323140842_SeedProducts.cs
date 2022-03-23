1dfde755ec9eda3183c3c0837932ec1a02e43511
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-23 11:17:01 -0300

    feat(data): add migrations to populate database

---
+++ CleanArchMvc.Infra.Data/Migrations/20220323140842_SeedProducts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" + 
+                "VALUES('Caderno espiral', 'Estojo escolar 100 folhas', 7.45, 50, 'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Estojo escolar', 'Estojo escolar cinza', 5.65, 60, 'estojo1.jpg', 1)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
