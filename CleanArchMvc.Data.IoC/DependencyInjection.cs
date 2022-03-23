ac85ef94882b49db6069bd4491c01ccba665b410
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2022-03-23 11:16:30 -0300

    feat: add DependencyInjection class for dependency injection container

---
+++ CleanArchMvc.Data.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Data.IoC
+{
+    public static class DependencyInjection
+    {
+        const string STRING_CONNECTION = "DefaultConnection";
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString(STRING_CONNECTION),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
