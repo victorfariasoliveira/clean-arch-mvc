f7fca1775d6c3ce9bdc346645d57b669fd18e029
Victor Oliveira <victorfariasdeoliveira@gmail.com>
2022-03-27 23:11:10 -0300

    feat: add concrete class CategoryService

---
+++ CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task CreateCategoryAsync(CategoryDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.CreateAsync(categoryEntity);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategoriesAsync()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategoriesAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task<CategoryDTO> GetCategoryByIdAsync(int? id)
+        {
+            var categoryEntity = await _categoryRepository.GetCategoryByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task RemoveCategoryAsync(int? id)
+        {
+            var categoryEntity = _categoryRepository.GetCategoryByIdAsync(id).Result;
+            await _categoryRepository.RemoveAsync(categoryEntity);
+        }
+
+        public async Task UpdateCategoryAsync(CategoryDTO categoryDto)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.UpdateAsync(categoryEntity);
+        }
+    }
+}
