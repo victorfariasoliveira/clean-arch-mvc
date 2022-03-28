cd7975c72fefb1737455ebf102f002b39dcfe285
Victor Oliveira <victorfariasdeoliveira@gmail.com>
2022-03-27 23:08:48 -0300

    feat: add ICategoryService and CategoryDTO to Application Layer

---
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        // Utilizando Data Annotations, são como validadores
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; private set; }
+
+        [Required(ErrorMessage = "The Description is Required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; private set; }
+
+        [Required(ErrorMessage = "The Price is Required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DisplayName("Price")]
+        public decimal Price { get; private set; }
+
+        [Required(ErrorMessage = "The Stock is Required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]  
+        public int Stock { get; private set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; private set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    internal interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<CategoryDTO> GetCategoryByIdAsync(int? id);
+        Task CreateCategoryAsync(CategoryDTO categoryDto);
+        Task UpdateCategoryAsync(CategoryDTO categoryDto);
+        Task RemoveCategoryAsync(int? id);
+    }
+}
